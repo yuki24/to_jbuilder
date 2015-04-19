5f4cc854a60223ee5a4d47f06d7b6c9cdbe91b87
Yuki Nishijima <mail@yukinishijima.net>
2015-04-19 12:56:23 -0700

    Add gems for debugging

--- Gemfile
+++ Gemfile
@@ -2,3 +2,6 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in to_jbuilder.gemspec
 gemspec
+
+gem 'byebug'
+gem 'did_you_mean'
--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -1,4 +1,6 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'to_jbuilder'
 
+require 'byebug'
+require 'did_you_mean'
 require 'minitest/autorun'
