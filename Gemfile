34467a855c24adad0f0dd1fd72820515bbfc6c39
Yuki Nishijima <mail@yukinishijima.net>
2015-04-25 17:25:31 -0700

    No need to use debugging tools on travis

--- Gemfile
+++ Gemfile
@@ -3,7 +3,10 @@ source 'https://rubygems.org'
 # Specify your gem's dependencies in to_jbuilder.gemspec
 gemspec
 
-gem 'byebug'
 gem 'pry'
-gem 'pry-byebug'
 gem 'did_you_mean'
+
+platforms :mri do
+  gem 'byebug'
+  gem 'pry-byebug'
+end
--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -1,6 +1,10 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'to_jbuilder'
 
-require 'byebug'
-require 'did_you_mean'
+begin
+  require 'byebug'
+  require 'did_you_mean'
+rescue LoadError
+end
+
 require 'minitest/autorun'
